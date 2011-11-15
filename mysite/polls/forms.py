67c08ba1f38fab7d34a8efcb79c91cf26b1d14a8
Harry <hjwp2@cantab.net>
2011-11-15 21:42:01 +0000

    deleting everything again for PART1

--- mysite/polls/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class PollVoteForm(object):
-    pass
--- mysite/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from polls.models import Poll
-
-def polls(request):
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polls.html', context)
-
