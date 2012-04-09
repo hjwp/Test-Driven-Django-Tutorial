1f9d80da8e1fd46b918094f06f2f4bcadf78d59f
Harry <hjwp2@cantab.net>
2012-04-09 16:35:39 +0100

    restarting with tute 1

---
+++ mysite/polls/tests.py
@@ -0,0 +1,24 @@
+import datetime
+from django.test import TestCase
+from polls.models import Poll
+
+class TestPollsModel(TestCase):
+    def test_creating_a_new_poll_and_saving_it_to_the_database(self):
+        # start by creating a new Poll object with its "question" set
+        poll = Poll()
+        poll.question = "What's up?"
+        poll.pub_date = datetime.datetime(2012, 12, 25)
+
+        # check we can save it to the database
+        poll.save()
+
+        # now check we can find it in the database again
+        all_polls_in_database = Poll.objects.all()
+        self.assertEquals(len(all_polls_in_database), 1)
+        only_poll_in_database = all_polls_in_database[0]
+        self.assertEquals(only_poll_in_database, poll)
+
+        # and check that it's saved its two attributes: question and pub_date
+        self.assertEquals(only_poll_in_database.question, "What's up?")
+        self.assertEquals(only_poll_in_database.pub_date, poll.pub_date)
+
--- mysite/polls/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from polls.tests.test_forms import *
-from polls.tests.test_models import *
-from polls.tests.test_views import *
-
