42a2c435f49338a82562176adafd15d8b3f58e3a
Harry <hjwp2@cantab.net>
2013-03-03 21:08:50 +0000

    miniscule progress on part 7 / choices

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,5 +4,5 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
 
-    def __unicode__(self):
+    def ___unicode__(self):
         return self.question
