3201d524ffe3cbcc81687c856be560bd19520717
Harry <hjwp2@cantab.net>
2013-03-04 23:21:26 +0000

    Add 500 template + sidebar for it, tweak headings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,4 +1,5 @@
 # Django settings for mysite project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
