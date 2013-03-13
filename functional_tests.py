3ce47731669ad448fecbece5da042bf499182d1c
Harry <hjwp2@cantab.net>
2013-03-13 20:34:45 +0000

    functional_tests.py with just the comments

--- functional_tests.py
+++ functional_tests.py
@@ -10,17 +10,11 @@ class PollsFunctionalTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-
     def test_voting_on_a_poll(self):
-        # Elspeth goest to check out a cool new polls site she's heard about
-        self.browser.get('http://localhost:8000')
-
-        # It is obviously all about polls:
-        self.assertIn('Poll', self.browser.title)
+        # Elspeth goes to check out a cool new polls site she's heard about
 
         # She clicks on the link to the first Poll, which is titled
         # "How awesome is TDD?"
-        self.fail('finish this test')
 
         # She is taken to a poll 'results' page, which says
         # "No-one has voted on this poll yet"
@@ -45,6 +39,39 @@ class PollsFunctionalTest(unittest.TestCase):
         # She votes again, and they go 66% - 33%
 
         # Satisfied, she goes back to sleep
+        pass
+
+
+    def test_can_create_a_new_poll_via_admin_site(self):
+        # Mo the administrator goes to the admin page
+
+        # He sees the familiar 'Django administration' heading
+
+        # He types in his username and passwords and hits return
+
+        # His username and password are accepted, and he is taken to
+        # the Site Administration page
+
+        # He sees a section named "Polls" with a model called "Polls" in it
+
+        # He clicks the second link, which takes him to the polls listing page
+        # which shows there are no polls yet
+
+        # He clicks the 'Add poll' link
+
+        # He types in an interesting question for the Poll
+
+        # He sets the date and time of publication - it'll be a new year's
+        # poll!
+
+        # He sees he can enter choices for the Poll.  He adds three
+
+        # Mo clicks the save button
+
+        # He is returned to the "Polls" listing, where he can see his
+        # new poll, listed as a clickable link
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
